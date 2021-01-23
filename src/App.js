3c30f65842fa051e2090e5464f3e9e833e286863
Mohammad Amiri <mohmd.a.a@gmail.com>
2021-01-23 11:31:59 -0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Mohammad Amiri</h1>
     </div>
   );
